6f3877dc81762e3d55874b72ab0985655424f5f2
riku213 <kotatorameme3@gmail.com>
2026-06-28 20:11:18 +0900

    feat: 検索結果一覧表示

--- BookReviewPlatform/urls.py
+++ BookReviewPlatform/urls.py
@@ -21,4 +21,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('search/', TemplateView.as_view(template_name='search.html'), name='search'),
 ]
